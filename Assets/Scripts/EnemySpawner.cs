397878bc9c685664a0bfc1890c4cdcd3f7b45c31
Young312886 <hongym0497@gmail.com>
2024-09-01 22:40:33 +0900

    Add : Enemy Spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] enemies;
+    private float[] arrPosX = { -2.2f, -1.1f, 0f, 1.1f, 2.2f };
+    // Start is called before the first frame update
+    void Start()
+    {   
+        foreach (float posX in arrPosX) {
+            int index = Random.Range(0, enemies.Length);
+            SpwanEnemey(posX, index);
+        }
+    }
+
+    void SpwanEnemey(float posX, int index) 
+    {
+        Vector3 spawnPos = new Vector3(posX, transform.position.y, transform.position.z);
+        Instantiate(enemies[index], spawnPos, Quaternion.identity);
+    }
+}
